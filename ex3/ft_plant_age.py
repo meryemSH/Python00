9ac59d2cc6c5f3d8c0fb439cb3354fc2efcb83ee
Meryem Seghrouchni Idrissi <mseghrou@c3r2p8.1337.ma>
2026-01-02 14:30:00 +0100

    push

---
+++ ex3/ft_plant_age.py
@@ -0,0 +1,6 @@
+def ft_plant_age():
+    plants_age = int(input("Enter plant age in days: "))
+    if plants_age > 60 :
+        print("Plant is ready to harvest!")
+    else : 
+        print("Plant needs more time to grow.")
